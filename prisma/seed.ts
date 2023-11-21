dcfed5bd32716696d51744b1cd521069420797d7
ali01 <astrid.lindfors@findity.com>
2023-11-21 20:24:31 +0100

    seed.ts

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -3,43 +3,7 @@
  */
 import prisma from '../src/prisma'
 
-async function main() {
-	/**
-	 * Publishers
-	 */
-	// const hutchinson = await prisma.publisher.upsert({
-	// 	where: { id: 1 },
-	// 	update: {},
-	// 	create: { id: 1, name: "Hutchinson" }
-	// })
-
-	/**
-	 * Authors
-	 */
-	// const clarke = await prisma.author.upsert({
-	// 	where: { id: 1 },
-	// 	update: {},
-	// 	create: { name: "Sir Arthur C. Clarke" }
-	// })
-
-	/**
-	 * Books
-	 */
-	// const odessey = await prisma.book.upsert({
-	// 	where: { id: 1 },
-	// 	update: {},
-	// 	create: {
-	// 		title: "2001: A Space Odessey",
-	// 		pages: 224,
-	// 		publisherId: hutchinson.id,
-	// 		authors: {
-	// 			connect: [
-	// 				{ id: clarke.id },
-	// 			],
-	// 		}
-	// 	}
-	// })
-}
+async function main() {}
 
 main()
 	.then(async () => {
